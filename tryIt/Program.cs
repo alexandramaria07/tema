2a54891c5da6a4870a8951a60f28238e49d5fcf8
MARIA-ALEXANDRA CRAIU <alexandra_mariaa27@yahoo.com>
2017-12-16 19:22:38 +0200

    new

--- tryIt/Program.cs
+++ tryIt/Program.cs
@@ -10,7 +10,7 @@ namespace tryIt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!!!"); 
+            Console.WriteLine("Hello friends!!!"); 
         }
     }
 }
